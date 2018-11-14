cb308b61bedd45c19f8aab5fb22e55a25ef64018
Elena Oonincx <potatokitty98@gmail.com>
2018-11-14 09:09:56 +0100

    added buttons
    knoppen worden automatisch weergegeven aan de hand van of ze bestaan of niet. En als je erop klikt wordt de content ervan weergegeven.

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -5,24 +5,60 @@
     session_start();
     $_SESSION['user'] = "student";
 ?>
+<script>
+function toggleDisplay($department, $department1)
+{
+    var x = document.getElementById($department);
+    var y = document.getElementById($department1);
+    console.log(y);
+    console.log(x);
+    if (x.style.display == "none" && y.style.display == "none")
+    {
+        x.style.display = "block";
+        y.style.display = "block";
+    }
+    else 
+    {
+        x.style.display = "none";
+        y.style.display = "none";
+    }
+}
+</script>
+
 <div class="mailPanel">
     <div class="mails">
         <?php 
             $mails = mailController::getMails();
             foreach($mails as $mail)
             {
-                echo "<button> " . $mail['department'] . "</button>";
+                $department = $mail['department'];
+                $department1 = $mail['department'] . "1";
+                echo "<button onClick='toggleDisplay(`$department`, `$department1`)'>" . $mail['department'] . "</button> \r\n";
             }
         ?>
     </div>
     <div class="currentMail">
         <div class="mailInfo">
-            <p>afzender: <?php $mail['department'];?></p>
-            <p>datum: <?php $mail['timeResponse'];?></p>
-            <p>aan: <?php $mail['groupNumber']; ?></p>
+            <?php 
+                foreach($mails as $mail)
+                {
+                    echo "<div id='" . $mail['department'] . "'>\r\n";
+                        echo "<p>afzender: " .  $mail['department'] . "</p>";
+                        echo "<p>datum: " . $mail['timeResponse'] . "</p>";
+                        echo "<p>aan: " . $mail['groupNumber'] . "</p>";
+                    echo "</div>";
+                }
+            ?>
         </div>
         <div class="mailContent">
-            <p></p>
+            <?php
+                foreach($mails as $mail)
+                {
+                    echo "<div id='" . $mail['department'] . "1'>\r\n";
+                        echo "<p>" . $mail['mailResponse'] . "</p>";
+                    echo "</div>";
+                } 
+            ?>
         </div>
     </div>
 </div>          
