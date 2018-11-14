651171ea10952b420c721672636b7991dbd367b0
Elena Oonincx <potatokitty98@gmail.com>
2018-11-14 16:26:11 +0100

    begin planningStudent
    begin gemaakt aan de planning van studenten. Voornamelijk gefocused op lay-out en alles netjes zetten.

--- resources/views/Student/student.blade.php
+++ resources/views/Student/student.blade.php
@@ -20,10 +20,21 @@
         <button id="openMail" onClick="window.location='/studentMail'">Open Mail</button>
         <button id="logout" onClick="window.location='/logout'">Logout</button>
     </div>
-    <div class="welcome">
-        <h1>Hallo Student</h1>
-    </div>
-    <div class="planning">
+    <div class="studentPlanning">
+        <div class="welcome">
+            <h1>Hallo Student</h1>
+        </div>
+        <div class="planning">
+            <p class="eightHr">8hr</p>
+            <p class="nineHr">9hr</p>
+            <p class="tenHr">10hr</p>
+            <p class="elevenHr">11hr</p>
+            <p class="twelveHr">12hr</p>
+            <button class="salesButton">Sales</button>
+            <button class="managerButton">Manager</button>
+            <button class="developmentButton">Development</button>
+            <button class="financeButton">Finances</button>
+        </div>
     </div>
 </div>
 <?php
