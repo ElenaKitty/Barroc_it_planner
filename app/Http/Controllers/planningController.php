a50bd055291c53d392a28a0befdddff6918ace51
Elena Oonincx <potatokitty98@gmail.com>
2018-11-30 08:30:40 +0100

    multiple Modals v1.0
    Zorgt ervoor dat meerdere modals op de correcte manier kunnen worden weergegeven. Dit is puur de styling en paar aparte methodes voor iedere afdeling.

--- app/Http/Controllers/planningController.php
+++ app/Http/Controllers/planningController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class planningController extends Controller
 {
-    public static function getMeetings()
+    public static function getAllMeetings()
     {
         $dbh = new \PDO('mysql:host=localhost;dbname=planner_barroc', 'root', '');
         $dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
@@ -19,4 +19,57 @@ class planningController extends Controller
         $result = $sth->fetchAll(\PDO::FETCH_ASSOC);
         return $result;
     }
+    public static function getSalesMeetings()
+    {
+        $dbh = new \PDO('mysql:host=localhost;dbname=planner_barroc', 'root', '');
+        $dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $dbh->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+        date_default_timezone_set("Europe/Amsterdam");
+        $date = date("d-m-y H:i:s");
+
+        $sth = $dbh->prepare("SELECT * FROM `meetings` WHERE department = 'Sales'");
+        $sth->execute();
+        $result = $sth->fetchAll(\PDO::FETCH_ASSOC);
+        return $result;
+    }
+    public static function getManagerMeetings()
+    {
+        $dbh = new \PDO('mysql:host=localhost;dbname=planner_barroc', 'root', '');
+        $dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $dbh->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+        date_default_timezone_set("Europe/Amsterdam");
+        $date = date("d-m-y H:i:s");
+
+        $sth = $dbh->prepare("SELECT * FROM `meetings` WHERE department = 'Manager'");
+        $sth->execute();
+        $result = $sth->fetchAll(\PDO::FETCH_ASSOC);
+        return $result;
+    }
+    public static function getDevelopmentMeetings()
+    {
+        $dbh = new \PDO('mysql:host=localhost;dbname=planner_barroc', 'root', '');
+        $dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $dbh->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+        date_default_timezone_set("Europe/Amsterdam");
+        $date = date("d-m-y H:i:s");
+
+        $sth = $dbh->prepare("SELECT * FROM `meetings` WHERE department = 'Development'");
+        $sth->execute();
+        $result = $sth->fetchAll(\PDO::FETCH_ASSOC);
+        return $result;
+    }
+    public static function getFinancesMeetings()
+    {
+        $dbh = new \PDO('mysql:host=localhost;dbname=planner_barroc', 'root', '');
+        $dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $dbh->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+        date_default_timezone_set("Europe/Amsterdam");
+        $date = date("d-m-y H:i:s");
+
+        $sth = $dbh->prepare("SELECT * FROM `meetings` WHERE department = 'Finances'");
+        $sth->execute();
+        $result = $sth->fetchAll(\PDO::FETCH_ASSOC);
+        return $result;
+    }
+
 }
